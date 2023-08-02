ff313e3de6fe256abf733b3112fb4e7ddab039f6
EK0720 <tuanbg123459876@gmail.com>
2023-08-02 16:20:19 +0700

    Floor2

---
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.21.0/Hello Cardboard/Scripts/MapPointer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPointer : MonoBehaviour
+{
+    public float m_spinSpeed;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AutoSpin();
+    }
+    void AutoSpin(){
+        transform.Rotate(0, m_spinSpeed, 0, Space.Self);
+    }
+    public void OnPointerEnter()
+    {
+        transform.localScale += new Vector3(0.03f,0.03f,0.03f);
+    }
+    public void OnPointerExit()
+    {
+        transform.localScale -= new Vector3(0.03f,0.03f,0.03f);
+    }
+    public void OnPointerClick()
+    {
+        Move();
+    }
+    void Move(){
+        player.transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+    }
+    void OnMouseDown()
+    {
+        Move();
+    }
+}
---
+++ Assets/Script/InfoManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoManager : MonoBehaviour
+{
+    public GameObject Panel;
+    private bool Show = false;
+    public void ShowAndHideInfo(){
+        if(!Show){
+            Panel.SetActive(true);
+            Show = true;
+        }
+        else {
+            Panel.SetActive(false);
+            Show = false;
+        }
+    }
+}
