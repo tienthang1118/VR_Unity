c4893c63022c7355040806e44c19bad125585f33
Khuat Manh <khuatdinhmanh2001@gmail.com>
2023-08-15 14:11:52 +0700

    update floor3, play/pause video, time, progessbar

--- Assets/Samples/Google Cardboard XR Plugin for Unity/1.21.0/Hello Cardboard/Scripts/MapPointer.cs
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.21.0/Hello Cardboard/Scripts/MapPointer.cs
@@ -6,14 +6,14 @@ public class MapPointer : MonoBehaviour
 {
     public GameObject player;
     private float spinSpeed = 1;
-    [SerializeField]private float gazeDuration;
+    [SerializeField] private float gazeDuration;
     private float gazeTime = 1.5f;
     private bool isGaze;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,34 +22,38 @@ public class MapPointer : MonoBehaviour
         Move();
         AutoSpin();
     }
-    void AutoSpin(){
+    void AutoSpin()
+    {
         transform.Rotate(0, spinSpeed, 0, Space.Self);
     }
     public void OnPointerEnter()
     {
         isGaze = true;
-        transform.localScale += new Vector3(0.03f,0.03f,0.03f);
+        transform.localScale += new Vector3(0.03f, 0.03f, 0.03f);
     }
     public void OnPointerExit()
     {
         isGaze = false;
         gazeDuration = 0;
-        transform.localScale -= new Vector3(0.03f,0.03f,0.03f);
+        transform.localScale -= new Vector3(0.03f, 0.03f, 0.03f);
     }
     public void OnPointerClick()
     {
-        
+
     }
-    void Move(){
-        if(isGaze){
+    void Move()
+    {
+        if (isGaze)
+        {
             gazeDuration += Time.deltaTime;
         }
-        if(gazeDuration>=gazeTime){
+        if (gazeDuration >= gazeTime)
+        {
             player.transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
         }
     }
     void OnMouseDown()
     {
-        
+
     }
 }
